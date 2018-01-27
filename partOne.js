a4cb9c85cfc1cc5ab52aafd1e8622b0379f89506
kailinandcode <35666977+kailinandcode@users.noreply.github.com>
2018-01-27 14:57:12 -0800

    Update partOne.js

--- partOne.js
+++ partOne.js
@@ -9,7 +9,8 @@ function tenStepLoop() {
   }
 }
 
-//MORE LINES. a) modify code so every line starts at canva's center
+//MORE LINES. 
+//a) modify code so every line starts at canva's center
 function setup() {
   createCanvas(340, 340);
 }
@@ -19,3 +20,20 @@ var center = 340/2;
 function draw() {
   line (center, center, random(width), random(height));
 }
+
+//b) modify code so every line ends at the top-right quadrant of the canvas
+function draw() {
+   line (center, center, random(center, 340), random(center, 0));
+}
+
+//c. modify code so that all lines have a slope of 2
+function draw() {
+  while(true) {
+    var randomX2 = int(random(center, 340));
+    var randomY2 = int(random(center, 0));
+    if ((randomY2 - center) * 2 == (randomX2 - center)) {
+      line (center, center, randomX2, randomY2);
+    }
+  }
+}
+
