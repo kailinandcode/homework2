6376b1bf89ae5b35479d6a3ce9c8784b76b17ce7
kailinandcode <35666977+kailinandcode@users.noreply.github.com>
2018-01-29 17:46:09 -0800

    Create mondrian.js

---
+++ mondrian.js
@@ -0,0 +1,5 @@
+//Assignment: Write code that makes an image in the style of Mondrian by drawing lines and colored rectangles.
+
+/*Assignment: Do something different. 
+Pick a particular feature of Mondrian's work--perhaps the angles, colors, shapes, or strokes, or something else--
+and modify it in your sketch. Give us your own take on Mondrian!*/
