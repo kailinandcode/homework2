b86de643a06ab90f133c2be0003da90dc90730b0
kailinandcode <35666977+kailinandcode@users.noreply.github.com>
2018-01-29 17:43:59 -0800

    Update fadingCircles.js

--- fadingCircles.js
+++ fadingCircles.js
@@ -6,6 +6,7 @@ Use this opportunity to explore a color palate.
 Change your circle sizes to match the emotion you would like to convey with your colors.
 */
 
+/*
 background(255);
 colorMode(HSB);
 noStroke();
@@ -13,5 +14,39 @@ noStroke();
 while(true) {
   background(0, 0, 255, 0.1);
   fill(random(360), 255, 255);
+  ellipse(random(width), random(height), random(20, 40));
+}
+*/
+
+//to change into pink-purple range!
+background(255);
+colorMode(RGB, 250, 100, 80);
+noStroke();
+
+while(true) {
+  background(0, 0, 255, 0.1);
+  fill(random(360), 25, 255);
+  ellipse(random(width), random(height), random(20, 40));
+}
+
+//to change into green-yellow range like the spring grass!
+background(255);
+colorMode(RGB, 250, 50, 80);
+noStroke();
+
+while(true) {
+  background(0, 0, 255, 0.1);
+  fill(random(360), 125, 0);
+  ellipse(random(width), random(height), random(20, 40));
+}
+
+//to change into black-gray neutral scheme:
+background(255);
+colorMode(HSB, 250, 50, 180);
+noStroke();
+
+while(true) {
+  background(0, 0, 255, 0.1);
+  fill(random(360), 75, 0);
   ellipse(random(width), random(height), random(20, 40));
 }
